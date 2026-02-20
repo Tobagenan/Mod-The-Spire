4a9446458e9ee47822952c58d07bae96a07480ca
Tobagenan <jacot01720@gmail.com>
2026-02-19 19:39:30 -0500

    Fixed basicmod

--- BasicMod-character/src/main/java/custommod/util/Sounds.java
+++
@@ -1,8 +0,0 @@
-package custommod.util;
-
-import static custommod.BasicMod.audioPath;
-
-public class Sounds {
-    public static String TEST_SOUND = audioPath("test.wav"); //Load audio using a given path
-    public static String ding; //Load audio from audioPath based on the field name
-}
